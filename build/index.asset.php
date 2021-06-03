d27466628179ab5810511186498137f5f75e8afc
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2021-06-03 13:49:07 +0300

    format, lint, build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'c3fa2863b98ed82ba2281ce256588bf8');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '9de520919d0de25fa9eed24881908803');
